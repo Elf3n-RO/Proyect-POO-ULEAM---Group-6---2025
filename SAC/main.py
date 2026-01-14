fb37edb245f235b60dd0a6fe47d9540f1d29597a
Undertakercrow <derlisacosta946@gmail.com>
2026-01-14 01:16:39 -0500

    changes 2

--- SAC/main.py
+++ SAC/main.py
@@ -20,4 +20,21 @@ def run_demo():
     p1 = Postulante('CÉDULA', '1712345678', 'Derlis Dario', 'Carranza Acosta', 850.0, datetime.now())
     p1.condicion_socioeconomica = 'SI'
     p1.determinarSegmentos()
+    p2 = Postulante('CÉDULA', '1712345679', 'Steven Sne yder', 'Catagua Cedeño', 820.0, datetime.now())
+    p2.merito_academico = 'SI'
+    p2.determinarSegmentos()
 
+    proceso.postulantes.extend([p1, p2])
+
+    print('--- Ejecutando asignación demo ---')
+    print(proceso.ejecutarAsignacion())
+    print(proceso.resolverEmpate())
+
+    for postulante in proceso.postulantes:
+        if postulante.asignaciones:
+            print(postulante.asignaciones[0].registrarAceptacionCupo())
+
+    print(proceso.generarReporteConsolidado())
+
+if __name__ == '__main__':
+    run_demo()
